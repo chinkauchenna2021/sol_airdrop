dd957c719f328e24f63bae9981169123898d2cfd
chinkauchenna2021 <uchennachinka4@gmail.com>
2025-07-05 21:53:24 +0100

    FIX: layout.tsx (auth) folder

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    typedRoutes: true,
+  },
+  images: {
+    domains: [
+      'pbs.twimg.com',
+      'abs.twimg.com',
+      'avatars.githubusercontent.com',
+    ],
+  },
+  webpack: (config: { resolve: { fallback: any } }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    }
+    return config
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
