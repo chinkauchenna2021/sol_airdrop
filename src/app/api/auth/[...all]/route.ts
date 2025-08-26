e328baef81a5134fdb9e0609ebf33f950e5d85f3
chinkauchenna2021 <uchennachinka4@gmail.com>
2025-08-26 19:44:41 +0100

    FIX: issues with nextjs betterauths

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -45,4 +45,4 @@
 import { auth } from "@/lib/better-auth-enhanced"
 import { toNextJsHandler } from "better-auth/next-js";
  
-export const { POST, GET } = toNextJsHandler(auth.handler);
+export const { POST, GET } = toNextJsHandler(auth);
